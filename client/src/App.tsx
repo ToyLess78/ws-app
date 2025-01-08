c03e2cb66ad7931a6eaf5fdf9a50d82c8bf416b6
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2025-01-08 22:01:34 +0200

    feat(client): implemented added new topic logic

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,24 +2,14 @@ import "./App.css";
 import { Auth, Chat } from "./components/components";
 import { toast, ToastContainer } from "react-toastify";
 import { socket, SocketContext } from "./context/socket";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useSessionStorage } from "./hooks/hooks.ts";
-import { Topic, User } from "./interfaces/interfaces.ts";
-import axios from "axios";
 
 const App: React.FC = () => {
   const {
-    saveChatToSession,
-    getChatFromSession,
-    getUserFromSession
+    getUserFromSession,
+    getChatFromSession
   } = useSessionStorage();
-
-  const [user, setUser] = useState<null | User>(null);
-  const [topicsList, setTopicsList] = useState<Topic[]>(getChatFromSession() || []);
-
-  useEffect(() => {
-    setUser(getUserFromSession());
-  }, [topicsList]);
 
   useEffect(() => {
     const handleSocketError = (response: { message: string }) => {
@@ -32,68 +22,15 @@ const App: React.FC = () => {
     };
   }, []);
 
-  const sortTopics = (item: Topic): void => {
-    const updatedTopics = topicsList.filter((topic) => topic._id !== item._id);
-    setTopicsList([item, ...updatedTopics]);
-  };
-  const getApi = async (item: Topic) => {
-    try {
-      let request = await axios.get("https://api.chucknorris.io/jokes/random");
+  const user = getUserFromSession();
+  const chat = getChatFromSession();
 
-      let newAnswer = {
-        text: request.data.value,
-        role: "bot",
-        timestamp: new Date(),
-        messageId: new Date().getTime().toString(),
-      };
-
-      const newTopicsList = topicsList.map(topic => {
-        if (topic._id === item._id) {
-          topic.messages = [...topic.messages, newAnswer];
-          return topic;
-        }
-        return topic;
-      });
-      saveChatToSession(newTopicsList);
-      const savedTopics = getChatFromSession();
-
-      setTopicsList(savedTopics);
-      sortTopics(item);
-
-    } catch (error) {
-      console.log("Something went wrong", error);
-      alert("Something went wrong !");
-    }
-  };
-
-  const sendMessage = (item: Topic, message: string): void => {
-    const newMessage = {
-      text: message,
-      role: "user",
-      timestamp: new Date().toISOString(),
-      messageId: new Date().getTime().toString(),
-    };
-
-    const newTopicsList = topicsList.map((topic) => {
-      if (topic._id === item._id) {
-        return {
-          ...topic,
-          messages: [...topic.messages, newMessage],
-        };
-      }
-      return topic;
-    });
-
-    saveChatToSession(newTopicsList);
-    setTopicsList(newTopicsList);
-    getApi(item);
-  };
   return (
     <SocketContext.Provider value={socket}>
       <>
         <ToastContainer/>
-        {user && topicsList ? (
-          <Chat user={user} chat={topicsList} sendMessage={sendMessage}/>
+        {user && chat ? (
+          <Chat user={user}/>
         ) : (
           <Auth/>
         )}
