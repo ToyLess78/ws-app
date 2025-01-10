1658ed62315f6dc7787a7892931edc2e42fb1113
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2025-01-10 19:49:55 +0200

    feat(client): update Chat component for send messages logic

--- client/src/components/chat/Chat.tsx
+++ client/src/components/chat/Chat.tsx
@@ -6,7 +6,6 @@ import { Topic, User } from "../../interfaces/interfaces";
 import { useSessionStorage } from "../../hooks/useSessionStorage.ts";
 import { toast } from "react-toastify";
 import { socket } from "../../context/socket.ts";
-import axios from "axios";
 
 interface ChatProps {
   user: User;
@@ -25,15 +24,16 @@ export const Chat: React.FC<ChatProps> = ({user}) => {
     setActiveTopic(topic);
   };
 
+  const sortTopicsByUpdatedAt = (topics: Topic[]): Topic[] => {
+    return [...topics].sort((a, b) => new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime());
+  };
+
   const handleTopicDeleted = (response: { success: boolean; topicId: string }) => {
     if (response.success) {
       const updatedTopics = topicsList.filter((topic) => topic._id !== response.topicId);
       setTopicsList(updatedTopics);
       setActiveTopic({});
       saveChatToSession(updatedTopics);
-      toast.success("Topic deleted successfully!");
-    } else {
-      toast.error("Failed to delete the topic.");
     }
   };
 
@@ -42,21 +42,34 @@ export const Chat: React.FC<ChatProps> = ({user}) => {
       const newTopicsList = [response.topic, ...topicsList];
       setTopicsList(newTopicsList);
       setActiveTopic(response.topic);
-
       saveChatToSession(newTopicsList);
-      toast.success(`New topic "${response.topic.name}" created successfully!`);
     }
   };
-
   const handleTopicUpdated = (response: { success: boolean; topic: Topic }) => {
     if (response.success) {
       const updatedTopics = topicsList.map((topic) =>
         topic._id === response.topic._id ? response.topic : topic
       );
-      setTopicsList(updatedTopics);
-      saveChatToSession(updatedTopics);
-      setActiveTopic(response.topic);
-      toast.success(`Topic "${response.topic.name}" updated successfully!`);
+
+      const sortedTopics = sortTopicsByUpdatedAt(updatedTopics);
+      let shouldShowToast = false;
+
+      setActiveTopic((prevState) => {
+        if ("_id" in prevState && prevState._id === response.topic._id) {
+          return response.topic;
+        }
+        shouldShowToast = true;
+        return prevState;
+      });
+
+      setTopicsList(sortedTopics);
+      saveChatToSession(sortedTopics);
+
+      if (shouldShowToast) {
+        setTimeout(() => {
+          toast.info(`New message in "${response.topic.name}"`);
+        }, 0);
+      }
     }
   };
 
@@ -64,62 +77,6 @@ export const Chat: React.FC<ChatProps> = ({user}) => {
     toast.error(`Error: ${response.message}`);
   };
 
-  const sortTopics = (item: Topic): void => {
-    const updatedTopics = topicsList.filter((topic) => topic._id !== item._id);
-    setTopicsList([item, ...updatedTopics]);
-  };
-  const getApi = async (item: Topic) => {
-    try {
-      let request = await axios.get("https://api.chucknorris.io/jokes/random");
-
-      let newAnswer = {
-        text: request.data.value,
-        role: "bot",
-        timestamp: new Date(),
-        messageId: new Date().getTime().toString(),
-      };
-
-      const newTopicsList = topicsList.map(topic => {
-        if (topic._id === item._id) {
-          topic.messages = [...topic.messages, newAnswer];
-          return topic;
-        }
-        return topic;
-      });
-      saveChatToSession(newTopicsList);
-      const savedTopics = getChatFromSession();
-
-      setTopicsList(savedTopics);
-      sortTopics(item);
-
-    } catch (error) {
-      console.log("Something went wrong", error);
-      alert("Something went wrong !");
-    }
-  };
-
-  const sendMessage = (item: Topic, message: string): void => {
-    const newMessage = {
-      text: message,
-      role: "user",
-      timestamp: new Date().toISOString(),
-      messageId: new Date().getTime().toString(),
-    };
-
-    const newTopicsList = topicsList.map((topic) => {
-      if (topic._id === item._id) {
-        return {
-          ...topic,
-          messages: [...topic.messages, newMessage],
-        };
-      }
-      return topic;
-    });
-
-    saveChatToSession(newTopicsList);
-    setTopicsList(newTopicsList);
-    getApi(item);
-  };
   useEffect(() => {
     socket.on("error", handleSocketError);
     socket.on("topicCreated", handleTopicCreated);
@@ -137,8 +94,12 @@ export const Chat: React.FC<ChatProps> = ({user}) => {
 
   return (
     <div className="chat">
-      <Sidebar user={user} chat={topicsList} setActiveTopic={onActiveTopic} activeTopic={activeTopic as Topic}/>
-      <Messenger topic={activeTopic as Topic} sendMessage={sendMessage}/>
+      <Sidebar
+        user={user}
+        chat={topicsList}
+        setActiveTopic={onActiveTopic}
+        activeTopic={activeTopic as Topic}/>
+      <Messenger topic={activeTopic as Topic}/>
     </div>
   );
 };
