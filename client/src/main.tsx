e6fcbcf74d345d7cb2f7965281f8fd43a64853ec
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2025-01-11 23:15:58 +0200

    chore(client): remove StrictMode

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,4 @@
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
@@ -7,9 +6,7 @@ import "./index.css";
 const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <GoogleOAuthProvider clientId={googleClientId}>
-      <App/>
-    </GoogleOAuthProvider>
-  </StrictMode>,
+  <GoogleOAuthProvider clientId={googleClientId}>
+    <App/>
+  </GoogleOAuthProvider>
 );
